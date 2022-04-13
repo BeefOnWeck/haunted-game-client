f7d2e63e26516388c56084a073ebd8cf891f285f
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-04-13 05:59:37 -0400

    Added delay to resource badge state change to make the bouncing animation smoother.

--- src/components/ResourceBadges.js
+++ src/components/ResourceBadges.js
@@ -4,48 +4,72 @@ import '@shoelace-style/shoelace/dist/components/animation/animation.js';
 
 function ResourceBadges({ block, timber, fiber, cereal, rock }) {
 
+  // For toggling the resource badge bouncing
   const [blockBounce, setBlockBounce] = useState(false);
   const [timberBounce, setTimberBounce] = useState(false);
   const [fiberBounce, setFiberBounce] = useState(false);
   const [cerealBounce, setCerealBounce] = useState(false);
   const [rockBounce, setRockBounce] = useState(false);
 
+  // Need to delay state change till after bouncing starts
+  const [delayedBlock, setDelayedBlock] = useState(null);
+  const [delayedTimber, setDelayedTimber] = useState(null);
+  const [delayedFiber, setDelayedFiber] = useState(null);
+  const [delayedCereal, setDelayedCereal] = useState(null);
+  const [delayedRock, setDelayedRock] = useState(null);
+
+  // Stagger the bouncing
   useEffect(() => {
-    setTimeout(() => setBlockBounce(true), 1000);
+    setTimeout(() => {
+      setBlockBounce(true);
+      setTimeout(() => setDelayedBlock(block), 535);
+    }, 1000);
   },[block]);
 
   useEffect(() => {
-    setTimeout(() => setTimberBounce(true), 1200);
+    setTimeout(() => {
+      setTimberBounce(true);
+      setTimeout(() => setDelayedTimber(timber), 535);
+    }, 1200);
   },[timber]);
 
   useEffect(() => {
-    setTimeout(() => setFiberBounce(true), 1400);
+    setTimeout(() => {
+      setFiberBounce(true);
+      setTimeout(() => setDelayedFiber(fiber), 535);
+    }, 1400);
   },[fiber]);
 
   useEffect(() => {
-    setTimeout(() => setCerealBounce(true), 1600);
+    setTimeout(() => {
+      setCerealBounce(true);
+      setTimeout(() => setDelayedCereal(cereal), 535);
+    }, 1600);
   },[cereal]);
 
   useEffect(() => {
-    setTimeout(() => setRockBounce(true), 1800);
+    setTimeout(() => {
+      setRockBounce(true);
+      setTimeout(() => setDelayedRock(rock), 535);
+    }, 1800);
   },[rock]);
 
   return html`
     <div id="resource-list">
       <sl-animation name="bounce" easing="linear" duration="1000" iterations="1" ?play=${blockBounce} @sl-finish=${() => setBlockBounce(false)}>
-        <div class="resource block">${block}</div>
+        <div class="resource block">${delayedBlock}</div>
       </sl-animation>
       <sl-animation name="bounce" easing="linear" duration="1000" iterations="1" ?play=${timberBounce} @sl-finish=${() => setTimberBounce(false)}>
-        <div class="resource timber">${timber}</div>
+        <div class="resource timber">${delayedTimber}</div>
       </sl-animation>
       <sl-animation name="bounce" easing="linear" duration="1000" iterations="1" ?play=${fiberBounce} @sl-finish=${() => setFiberBounce(false)}>
-        <div class="resource fiber">${fiber}</div>
+        <div class="resource fiber">${delayedFiber}</div>
       </sl-animation>
       <sl-animation name="bounce" easing="linear" duration="1000" iterations="1" ?play=${cerealBounce} @sl-finish=${() => setCerealBounce(false)}>
-        <div class="resource cereal">${cereal}</div>
+        <div class="resource cereal">${delayedCereal}</div>
       </sl-animation>
       <sl-animation name="bounce" easing="linear" duration="1000" iterations="1" ?play=${rockBounce} @sl-finish=${() => setRockBounce(false)}>
-        <div class="resource rock">${rock}</div>
+        <div class="resource rock">${delayedRock}</div>
       </sl-animation>
     </div>
 
