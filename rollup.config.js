e173ed9c2880983e566e47ea0dc2ecaa26bb4b94
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-05-14 15:29:45 -0400

    Preparing for deployment

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 import path from 'path';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
 import copy from 'rollup-plugin-copy';
 import css from 'rollup-plugin-css-only';
-// import analyze from 'rollup-plugin-analyzer';
-// import { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 
 import process from 'process';
 import dotenv from 'dotenv'
@@ -16,7 +14,7 @@ dotenv.config();
 export default {
   input: 'src/index.js',
   output: {
-    file: './build.js',
+    file: './dist/build.js',
     format: 'es'
   },
   plugins: [
@@ -28,34 +26,26 @@ export default {
       browser: true,
     }),
     commonjs(),
-    // Bundle styles into bundle.css
     css({
       output: 'bundle.css' 
     }),
-    // Copy Shoelace assets to shoelace
     copy({
       targets: [
         {
           src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
-          dest: path.resolve(__dirname, '.')
+          dest: path.resolve(__dirname, './dist')
+        },
+        {
+          src: path.resolve(__dirname, './index.html'),
+          dest: path.resolve(__dirname, './dist')
+        },
+        {
+          src: path.resolve(__dirname, './src/style.css'),
+          dest: path.resolve(__dirname, './dist')
         }
       ]
     }),
-    // terser(),
-    // analyze(),
-    // babel({
-    //   include: ['**.js', 'node_modules/**'],
-    //   // babelHelpers: 'bundled',
-    //   presets: ['@babel/preset-env'],
-    //   plugins: ["@babel/plugin-transform-runtime"]
-    // }),
-    // copy({
-    //   targets: [
-    //     {
-    //       src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
-    //       dest: path.resolve(__dirname, 'dist/shoelace')
-    //     }
-    //   ]
-    // }),
-  ]
+    terser()
+  ],
+  preserveEntrySignatures: false,
 };
